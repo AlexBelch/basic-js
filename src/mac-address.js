be3fc9d8f64420e54be95e1945c5f8409da194a1
Alexander BELCHITSKIY <alexander.belchitskiy@1000geeks.com>
2022-04-29 00:52:03 +0300

    feat: add solution for subtask "(ST) Is MAC-48 Adress?"

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,10 +14,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const arr = n.split("-");
+  const symbols = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+
+  let result = true;
+
+  arr.every((element) => {
+    if (element.length !== 2) {
+      result = false;
+      return result;
+    }
+
+    element.split("").every((item) => {
+      if (!symbols.includes(item)) {
+        result = false;
+        return result;
+      }
+    });
+  });
+
+  return result;
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
