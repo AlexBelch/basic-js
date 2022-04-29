94fa44ee0dcc315e8e42c03cb5a7011244d50973
Alexander BELCHITSKIY <alexander.belchitskiy@1000geeks.com>
2022-04-29 16:06:01 +0300

    feat: add solution for subtask "(ST) Minesweeper"

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+
+  matrix.forEach((elementArr) => {
+    result.push(
+      elementArr.map((element) => {
+        return 0;
+      })
+    );
+  });
+
+  const lengthI = matrix.length - 1;
+
+  for (let i = 0; i <= lengthI; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const lengthJ = matrix[i].length - 1;
+
+      result[i][j] =
+        (j < lengthJ && matrix[i][j + 1] ? 1 : 0) +
+        (j < lengthJ && i < lengthI && matrix[i + 1][j + 1] ? 1 : 0) +
+        (i < lengthI && matrix[i + 1][j] ? 1 : 0) +
+        (j > 0 && i < lengthI && matrix[i + 1][j - 1] ? 1 : 0) +
+        (j > 0 && matrix[i][j - 1] ? 1 : 0) +
+        (i > 0 && j > 0 && matrix[i - 1][j - 1] ? 1 : 0) +
+        (i > 0 && matrix[i - 1][j] ? 1 : 0) +
+        (i > 0 && j < lengthJ && matrix[i - 1][j + 1] ? 1 : 0);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
