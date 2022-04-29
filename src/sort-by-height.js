a54f91c54ee769ef788a1ea1f6d5a2da957bed91
Alexander BELCHITSKIY <alexander.belchitskiy@1000geeks.com>
2022-04-29 13:39:53 +0300

    feat: add solution for subtask "(ST) Sort by height"

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const arrInd1 = [];
+  let newArr = arr
+    .filter((element, index) => {
+      if (element === -1) {
+        arrInd1.push(index);
+        return false;
+      }
+      return true;
+    })
+    .sort(function (a, b) {
+      return a - b;
+    });
+
+  arrInd1.forEach((element) => {
+    newArr.splice(element, 0, -1);
+  });
+
+  return newArr;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
